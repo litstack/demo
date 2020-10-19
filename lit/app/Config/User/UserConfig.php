2aa1c08bc958d0c7923deb7f3b87d38421083142
cbl <lennart.carbe@gmail.com>
2020-10-19 09:37:39 +0200

    wip

--- lit/app/Config/User/UserConfig.php
+++ lit/app/Config/User/UserConfig.php
@@ -4,7 +4,6 @@ namespace Lit\Config\User;
 
 use Ignite\Crud\Config\CrudConfig;
 use Ignite\Crud\CrudIndex;
-use Ignite\Crud\CrudShow;
 use Ignite\Page\Table\ColumnBuilder;
 use Illuminate\Support\Facades\Route;
 use Lit\Http\Controllers\User\UserController;
@@ -58,7 +57,7 @@ class UserConfig extends CrudConfig
     public function index(CrudIndex $page)
     {
         $page->table(fn ($table) => $this->indexTable($table))
-            ->query(fn ($query) => $query->with('ordered_roles'))
+            ->query(fn ($query)  => $query->with('ordered_roles'))
             ->sortByDefault('id.desc')
             ->search('username', 'first_name', 'last_name', 'email');
     }
@@ -88,44 +87,5 @@ class UserConfig extends CrudConfig
             ->label('')
             ->link(false)
             ->small();
-    }
-
-    /**
-     * Crud show container.
-     *
-     * @param  CrudShow $page
-     * @return void
-     */
-    public function show(CrudShow $page)
-    {
-        $page->card(function ($form) {
-            $form->input('first_name')
-                ->width(1 / 2)
-                ->creationRules('required')
-                ->rules('min:2', 'max:255')
-                ->title(ucwords(__lit('base.first_name')));
-
-            $form->input('last_name')
-                ->width(1 / 2)
-                ->creationRules('required')
-                ->rules('min:2', 'max:255')
-                ->title(ucwords(__lit('base.last_name')));
-
-            $form->input('email')
-                ->width(1 / 2)
-                ->creationRules('required')
-                ->rules('email:rfc,dns', 'unique:lit_users,email')
-                ->title('E-Mail');
-
-            $form->input('username')
-                ->width(1 / 2)
-                ->creationRules('required')
-                ->rules('min:2', 'max:60', 'unique:lit_users,username')
-                ->title(ucwords(__lit('base.username')));
-
-            $form->password('password')
-                ->title(ucwords(__lit('base.password')))
-                ->width(1 / 2);
-        });
     }
 }

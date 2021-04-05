a711ee2076db78accf5b40dd8c4d79843621dc27
cbl <lennart.carbe@gmail.com>
2021-04-05 17:10:24 +0200

    fix

--- lit/app/Config/SearchConfig.php
+++ lit/app/Config/SearchConfig.php
@@ -3,12 +3,13 @@
 namespace Lit\Config;
 
 use Ignite\Search\Search;
-use Ignite\Search\SearchConfig as Config;
+// use Ignite\Search\SearchConfig as Config;
 use Lit\Config\Crud\CustomerConfig;
 use Lit\Config\Crud\OrderConfig;
 use Lit\Config\Crud\ProductConfig;
 
-class SearchConfig extends Config
+// class SearchConfig extends Config
+class SearchConfig
 {
     public function main(Search $search)
     {
